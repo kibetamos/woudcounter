06d6e2c927be50180653b4d95d93fd5b4c91c88f
kibet kilel <amos.rotich@strathmore.edu>
2021-07-30 16:25:36 +0300

    updated

--- venus/myapp/views.py
+++ venus/myapp/views.py
@@ -18,6 +18,6 @@ def index(request):
 def counter(request):
     # this gets the words from the name of the form in the index.html
     # if e change the name of the text are , we have o change the variable as well
-    text = request.GET['text']
+    text = request.POST['text']
     number_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': number_of_words})
