33e59b79a1f9017ae25c822eca83abe4edef11b1
kibet kilel <amos.rotich@strathmore.edu>
2022-05-18 15:24:05 +0300

    Add vercel hosting

--- venus/venus/settings.py
+++ venus/venus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-**&cde)c5vgpvn8^hn3ft41c-qnn$amkc3nud#(is1wi9enqxu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'venus.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
